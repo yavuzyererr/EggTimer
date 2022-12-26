432d82d5439df110d1ce435b635b45b5f7f8e821
yavuz yerer <yavuz.yererr@gmail.com>
2022-12-26 17:09:52 +0300

    New Project

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Yavuz Yerer on 26.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
